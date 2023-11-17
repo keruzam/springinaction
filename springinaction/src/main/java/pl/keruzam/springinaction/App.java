71f557053324e06cfe5f5ce9e60ee08b979c1346
Tomasz Mazurek <mazurek.zg@gmail.com>
2023-11-17 22:22:08 +0100

    add jsf dependency

--- springinaction/src/main/java/pl/keruzam/springinaction/App.java
+++ springinaction/src/main/java/pl/keruzam/springinaction/App.java
@@ -1,7 +1,10 @@
 package pl.keruzam.springinaction;
 
+import javax.faces.context.FacesContext;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.jsf.FacesContextUtils;
 
 public class App {
 
@@ -12,6 +15,9 @@ public class App {
 		HelloWorld helloWorld2 = (HelloWorld) context.getBean("helloWorld");
 		helloWorld.getMessage();
 		helloWorld2.getMessage();
+		
+		
+		//ApplicationContext facesCtx = FacesContextUtils.getWebApplicationContext(FacesContext.getCurrentInstance());
 	}
 
 }
